02ffa7eb5a64be3f4199d83f5a3cf51b41295672
dingdongzhao <ddzyan@163.com>
2022-10-18 11:10:09 +0800

    feat: 增加访问日志

---
+++ src/app/service/admin/sys/reqLog.ts
@@ -0,0 +1,30 @@
+import { Provide, Inject } from '@midwayjs/decorator';
+
+import { BaseService } from '../../../../core/baseService';
+import { ReqLogMapping } from '../../../mapping/reqLog';
+
+@Provide()
+export class ReqLogService extends BaseService {
+  @Inject()
+  protected mapping: ReqLogMapping;
+
+  async save(
+    url: string,
+    params: string,
+    status: number,
+    consumeTime: number,
+    method: string | undefined,
+    adminId: number | null
+  ): Promise<void> {
+    const ip = this.utils.getReqIP(this.ctx);
+    await this.mapping.saveNew({
+      action: url,
+      param: JSON.stringify(params),
+      adminId: adminId || 1,
+      ip,
+      method: method ? method.toUpperCase() : undefined,
+      status,
+      consumeTime,
+    });
+  }
+}
