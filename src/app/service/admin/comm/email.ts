fdcf6dbcdda2a49efc4f35ff280e271dc0bc7481
dingdongzhao <ddzyan@163.com>
2022-10-20 17:18:17 +0800

    优化task异常邮件通知

---
+++ src/app/service/admin/comm/email.ts
@@ -0,0 +1,79 @@
+import { Provide, Inject, Init, Logger } from '@midwayjs/decorator';
+import { Clients, Metadata } from '@midwayjs/grpc';
+
+import { email } from '../../../../domain/email';
+import { BaseService } from '../../../../core/baseService';
+
+@Provide()
+export class EmailService extends BaseService {
+  @Inject()
+  private grpcClients: Clients;
+
+  private emailGrpcService: email.EmailClient;
+
+  private readonly timeOut = 10000;
+
+  private meta: Metadata;
+
+  @Logger()
+  private logger;
+
+  @Init()
+  async init() {
+    // 赋值一个服务实例
+    this.emailGrpcService =
+      this.grpcClients.getService<email.EmailClient>('email.Email');
+
+    this.meta = new Metadata();
+  }
+
+  // 发送邮件
+  async sendEmail(param: email.SendEmailRequest): Promise<string> {
+    try {
+      const { emailType, toEmailAddress, replaceContent, cacheValue } = param;
+
+      const { reqId } = this.ctx;
+      this.meta.add('reqId', reqId);
+
+      const result = await this.emailGrpcService
+        .sendEmail({
+          timeout: this.timeOut,
+          metadata: this.meta,
+        })
+        .sendMessage({
+          emailType,
+          toEmailAddress,
+          replaceContent,
+          cacheValue,
+        });
+
+      this.logger.info(
+        `email address ${param.toEmailAddress} emailType ${emailType} send success`,
+        result
+      );
+
+      // 返回结果
+      return result.code;
+    } catch (error) {
+      this.logger.error(
+        `email address ${param.toEmailAddress} send error`,
+        error
+      );
+    }
+  }
+
+  // 群发通知邮件
+  async sendEmails(
+    param: email.SendEmailRequest & { emails: string[] }
+  ): Promise<void> {
+    const { emails, emailType, replaceContent, cacheValue } = param;
+    emails.forEach(toEmailAddress => {
+      this.sendEmail({
+        emailType,
+        toEmailAddress,
+        replaceContent,
+        cacheValue,
+      });
+    });
+  }
+}
